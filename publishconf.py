e4aa404eb52e402e18279a6a3a3b6a8c5c03b134
Christopher Orlick <corlick@ualberta.ca>
2022-10-12 16:39:32 -0600

    test2

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'corlick98.github.io'
+SITEURL = 'https://corlick98.github.io/cmput-404-lab-5/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
